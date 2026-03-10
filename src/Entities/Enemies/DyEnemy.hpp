e75d0df596bf45ba57797e3132b82c8b26867720
ChristianNavarro286 <christian.navarro4@upr.edu>
2026-03-09 20:58:23 -0400

    Add random texture variation for DyEnemy

--- src/Entities/Enemies/DyEnemy.hpp
+++ src/Entities/Enemies/DyEnemy.hpp
@@ -6,12 +6,24 @@ class DyEnemy : public Enemy {
         float angle = 135;
         float aimAngle = 225;
         bool loop = false;
+        Rectangle texture;  // Stores the sprite used to draw the enemy
 
     public:
         DyEnemy(float x, float y) : Enemy(x, y) { 
             this->cooldown = GetRandomValue(90, 300);
             this->health = 1; 
             this->points = 150;
+
+            // Randomly selects one of two textures when the enemy is created
+
+            if (GetRandomValue(0, 1) == 0)
+            {
+                texture = {2, 128, 13, 14};
+            }
+            else
+            {
+                texture = {2, 147, 13, 13};
+            }
         }
 
         void draw() override;
